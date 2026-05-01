e2a5159c8b78b1e64f67aba138bfa9932dce1ccb
FransiscoP <crazyashtro@gmail.com>
2026-05-01 01:16:13 -0400

    Add API key management via AppState

--- src/main/java/edu/farmingdale/csc325capstone/service/AppState.java
+++ src/main/java/edu/farmingdale/csc325capstone/service/AppState.java
@@ -1,5 +1,5 @@
 package edu.farmingdale.csc325capstone.service;
 
 public class AppState {
-    public static String openAiApiKey;
+    public static String openAiApiKey = "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";
 }
